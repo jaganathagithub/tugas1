dd2e3b8defc2c6afabe6a01daa0ec3302579763b
jaganathagithub <160811425+jaganathagithub@users.noreply.github.com>
2024-09-30 10:32:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tugas1</title>
+</head>
+<body>
+    <h1>Selamat Datang</h1>
+    <p>Nama: Jaganatha</p>
+    <p>NPM : 22313004</p>
+    <p>Selamat Datang di Matkul Pemrograman Web Beorientasi Layanan</p>
+</body>
+</html>
